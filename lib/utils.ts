e3b301a62938443729ef2bf48ffd404f92fbd5e2
ZaydBk <zaydme8@gmail.com>
2021-12-01 14:11:48 +0100

    fix graphql authorization

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { request } from 'graphql-request';
 
-export const shopFetcher = (query: string, variables: any, locale: string) => request(`${process.env.API_URL}`, query, {
+export const shopFetcher = (query: string, variables: any, locale: string, requestHeaders: any) => request(`${process.env.API_URL}`, query, {
     ...variables,
     _locale:locale
-  });
+  }, requestHeaders);
   
