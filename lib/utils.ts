bd447b65c9ba66331b74d270aa34702f0c1d73e7
ZaydBk <zaydme8@gmail.com>
2021-12-02 20:34:44 +0100

    fix env, added fallback

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { request } from 'graphql-request';
+import { API_URL } from 'constants/graphql';
 
-export const shopFetcher = (query: string, variables: any, locale: string, requestHeaders: any) => request(`${process.env.API_URL}`, query, {
+export const shopFetcher = (query: string, variables: any, locale: string, requestHeaders: any) => request(`${API_URL}`, query, {
     ...variables,
     _locale:locale
   }, requestHeaders);
