0689aeb4a827454892771815b6fe95c10f6b0de7
mjkohan <mjk1551381@gmail.com>
2024-04-22 23:32:45 +0330

    style for responses, todo: price, time , and after select

---
+++ components/Rates/RateBoxItem.tsx
@@ -0,0 +1,34 @@
+import {Box, Button, Typography} from "@mui/material";
+
+export default function RateBoxItem({item, idx}) {
+    return (
+        <Box sx={{
+            width: "100%",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: "space-between",
+            borderTop: idx === 0 ? "none" : "black solid 1px",
+            p: 5
+        }}>
+            <div>
+                <Typography fontWeight={700} variant="h5" component="h5">
+                    ARRIVES ON
+                </Typography>
+                <Typography fontSize={16} color={"lightslategray"} variant="h6" component="h6">
+                    {item.operationalDetail?.commitDate ?? "can not be calculated"}
+                </Typography>
+            </div>
+
+            <Typography variant="h5" component="h5">
+                {item.serviceName}
+            </Typography>
+
+            <Button size={"large"} variant="contained" color="warning">
+                <Typography fontWeight={700} variant="h5" component="h5">
+                    {item.ratedShipmentDetails[0].totalNetFedExCharge}$
+                </Typography>
+
+            </Button>
+        </Box>
+    )
+}
