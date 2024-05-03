ccc71dcf603663b08305934cb8775508a9cf4715
mjkohan <mjk1551381@gmail.com>
2024-05-03 22:32:27 +0330

    ui changes

--- components/create-shipment/Toggle.js
+++ components/create-shipment/Toggle.js
@@ -2,38 +2,54 @@ import React, { useState } from 'react'
 
 const Toggle = ({unit,setUnit}) => {
 
+    const handleChange = (event) => {
+        setUnit(event.target.value);
+    };
 
     return (
-        <div className={"flex justify-center items-center"}>
-            {/*<div className={"font-bold text-2xl mr-8"}>*/}
-            {/*    Unit*/}
-            {/*</div>*/}
-            <label className='themeSwitcherTwo shadow-card relative inline-flex cursor-pointer select-none items-center justify-center rounded-md bg-white p-1'>
-                <input
-                    type='checkbox'
-                    className='sr-only'
-                    checked={unit === 'metric'}
-                    onChange={()=>setUnit(unit === 'metric' ? 'imperial' : 'metric')}
-                />
-                <span
-                    className={`flex items-center space-x-[6px] rounded py-2 px-[18px] text-sm font-medium ${
-                        (unit === 'metric') ? 'text-primary bg-blue-600 text-white' : 'text-body-color'
-                    }`}
-                >
-
-          Metric(kg,cm)
-        </span>
-                <span
-                    className={`flex items-center space-x-[6px] rounded py-2 px-[18px] text-sm font-medium ${
-                        !(unit === 'metric') ? 'text-primary bg-blue-600 text-white' : 'text-body-color'
-                    }`}
-                >
-
-          Imperial(lb,in)
-        </span>
-            </label>
+        <div>
+            <select
+                className="bg-gray-50 mr-4 border border-gray-300 font-bold text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value={unit}
+                onChange={handleChange}
+            >
+                <option value="metric">Metric(kg,cm)</option>
+                <option value="imperial">Imperial(lb,in)</option>
+            </select>
         </div>
-    )
+    );
+
+    // return (
+    //     <div className={"flex justify-center items-center"}>
+    //         {/*<div className={"font-bold text-2xl mr-8"}>*/}
+    //         {/*    Unit*/}
+    //         {/*</div>*/}
+    //         <label className='themeSwitcherTwo shadow-card relative inline-flex cursor-pointer select-none items-center justify-center rounded-md bg-white p-1'>
+    //             <input
+    //                 type='checkbox'
+    //                 className='sr-only'
+    //                 checked={unit === 'metric'}
+    //                 onChange={()=>setUnit(unit === 'metric' ? 'imperial' : 'metric')}
+    //             />
+    //             <span
+    //                 className={`flex items-center space-x-[6px] rounded py-2 px-[18px] text-sm font-medium ${
+    //                     (unit === 'metric') ? 'text-primary bg-blue-600 text-white' : 'text-body-color'
+    //                 }`}
+    //             >
+    //
+    //       Metric(kg,cm)
+    //     </span>
+    //             <span
+    //                 className={`flex items-center space-x-[6px] rounded py-2 px-[18px] text-sm font-medium ${
+    //                     !(unit === 'metric') ? 'text-primary bg-blue-600 text-white' : 'text-body-color'
+    //                 }`}
+    //             >
+    //
+    //       Imperial(lb,in)
+    //     </span>
+    //         </label>
+    //     </div>
+    // )
 }
 
 export default Toggle
--- pages/create_shipment.js
+++ pages/create_shipment.js
@@ -7,19 +7,21 @@ import {useForm} from 'react-hook-form'
 import {useState} from "react";
 import Package from "../components/create-shipment/Package";
 import Toggle from "../components/create-shipment/Toggle";
+import {COUNTRIES} from "../constants";
 
 const Home = ({}) => {
     const {
         register, handleSubmit, getValues, formState: {errors},
     } = useForm()
     const [unit, setUnit] = useState('metric')
+    const [addressType, setAddressType] = useState('new_Address')
 
     const [packages, setPackages] = useState([{
         length: '',
         width: '',
         height: '',
         weight: '',
-        type:'box_rigid'
+        type: 'box_rigid'
     }])
     const onSubmit = (data) => {
         console.log(data)
@@ -31,7 +33,7 @@ const Home = ({}) => {
             width: '',
             height: '',
             weight: '',
-            type:'box_rigid'
+            type: 'box_rigid'
         })
         setPackages(temp)
     }
@@ -48,9 +50,36 @@ const Home = ({}) => {
     return (
         <Layout>
             <form onSubmit={handleSubmit(onSubmit)} className="container mx-auto p-4">
-                <h1 className="text-2xl font-bold mb-4">
+                <h1 className="text-6xl font-bold text-center mb-16">
                     Create a Shipping Label
                 </h1>
+                <h2 className="text-xl font-bold mb-4">
+                    Ship To
+                </h2>
+                <div className="grid grid-cols-4 gap-4 mb-4">
+                    <select {...register('country', {required: true, maxLength: 50})}
+                            className="border border-gray-400 p-2 rounded">
+                        {COUNTRIES.map((country,idx)=>{
+                            return(<option key={idx} value={country.value}>
+                                {country.label}
+                            </option>)
+                        })}
+                    </select>
+                    <input {...register('city', {required: true, maxLength: 50})}
+                           className="border border-gray-400 p-2 rounded" placeholder="City" type="text"/>
+                    <input {...register('state', {required: true, maxLength: 50})}
+                           className="border border-gray-400 p-2 rounded" placeholder="State" type="text"/>
+                    <input {...register('zipcode', {required: true, maxLength: 50})}
+                           className="border border-gray-400 p-2 rounded" placeholder="Zipcode" type="text"/>
+
+                </div>
+                <div className="grid grid-cols-2 gap-4 mb-4">
+                    <input {...register('address', {required: true, maxLength: 500})}
+                           className="border border-gray-400 p-2 rounded" placeholder="Address" type="text"/>
+                    <input {...register('address_opt', {required: false, maxLength: 50})}
+                           className="border border-gray-400 p-2 rounded"
+                           placeholder="Apt / Unit / Suite / etc. (optional)" type="text"/>
+                </div>
                 <div className="grid grid-cols-2 gap-4 mb-4">
                     <input {...register('email', {required: false, maxLength: 50})}
                            className="border border-gray-400 p-2 rounded" placeholder="Email (optional)" type="email"/>
@@ -63,106 +92,100 @@ const Home = ({}) => {
                     <input {...register('company', {required: false, maxLength: 50})}
                            className="border border-gray-400 p-2 rounded" placeholder="Company (optional)" type="text"/>
                 </div>
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                    <input {...register('address', {required: true, maxLength: 500})}
-                           className="border border-gray-400 p-2 rounded" placeholder="Address" type="text"/>
-                    <input {...register('address_opt', {required: false, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded"
-                           placeholder="Apt / Unit / Suite / etc. (optional)" type="text"/>
-                </div>
-                <div className="grid grid-cols-4 gap-4 mb-4">
-                    <input {...register('city', {required: true, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded" placeholder="City" type="text"/>
-                    <input {...register('state', {required: true, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded" placeholder="State" type="text"/>
-                    <input {...register('zipcode', {required: true, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded" placeholder="Zipcode" type="text"/>
-                    <select {...register('country', {required: true, maxLength: 50})}
-                            className="border border-gray-400 p-2 rounded">
-                        <option>
-                            United States
-                        </option>
-                    </select>
-                </div>
+
+
                 <h2 className="text-xl font-bold mb-4">
                     Ship From
                 </h2>
-                <select {...register('address_type', {required: true, maxLength: 50})}
+                <select onChange={(e)=>setAddressType(e.target.value)}
                         className="border border-gray-400 p-2 rounded mb-4">
-                    <option>
+                    <option value={'saved_Address'}>
                         Don't use saved Ship From Address
                     </option>
-                    <option>
+                    <option selected value={'new_Address'}>
                         Create New Ship From Address
                     </option>
                 </select>
-                <h3 className="text-lg font-bold mb-2">
-                    Physical Address
-                    <span className="text-blue-500">
-     Learn more
-    </span>
-                </h3>
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                    <input {...register('phy_name', {required: false, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded" placeholder="Name (optional)" type="text"/>
-                    <input {...register('phy_company', {required: false, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded" placeholder="Company (optional)" type="text"/>
-                </div>
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                    <input {...register('phy_address', {required: true, maxLength: 500})}
-                           className="border border-gray-400 p-2 rounded" placeholder="Address" type="text"/>
-                    <input {...register('phy_address_opt', {required: false, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded"
-                           placeholder="Apt / Unit / Suite / etc. (optional)" type="text"/>
-                </div>
-                <div className="grid grid-cols-4 gap-4 mb-4">
-                    <input {...register('phy_city', {required: true, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded" placeholder="City" type="text"/>
-                    <select {...register('phy_city1', {required: true, maxLength: 50})}
-                            className="border border-gray-400 p-2 rounded">
-                        <option>
-                            Alabama
-                        </option>
-                    </select>
-                    <input {...register('phy_zipcode', {required: true, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded" placeholder="Zipcode" type="text"/>
-                    <input {...register('phy_phone', {required: true, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded" placeholder="Phone" type="text"/>
-                </div>
-                <div className="mb-4">
-                    <input {...register('return_address', {required: false})} className="mr-2"
-                           type="checkbox"/>
-                    Use this address as the Return Address on my shipping labels
-                </div>
-                <div className="mb-4">
-                    <input {...register('save_ship', {required: false})} className="mr-2"
-                           type="checkbox"/>
-                    Save Ship From Address
-                    <input {...register('ship_nickname', {required: true, maxLength: 50})}
-                           className="border border-gray-400 p-2 rounded ml-2"
-                           placeholder="Nickname this Ship From Address" type="text"/>
-                </div>
+                {addressType !== 'new_Address' && <div className={"w-full"}>
+                    <h3 className="text-lg font-semibold mb-2">
+                        Physical Address
+                    {/*    <span className="text-blue-500">*/}
+                    {/*    Learn more*/}
+                    {/*</span>*/}
+                    </h3>
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                        <input {...register('phy_name', {required: false, maxLength: 50})}
+                               className="border border-gray-400 p-2 rounded" placeholder="Name (optional)"
+                               type="text"/>
+                        <input {...register('phy_company', {required: false, maxLength: 50})}
+                               className="border border-gray-400 p-2 rounded" placeholder="Company (optional)"
+                               type="text"/>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                        <input {...register('phy_address', {required: true, maxLength: 500})}
+                               className="border border-gray-400 p-2 rounded" placeholder="Address" type="text"/>
+                        <input {...register('phy_address_opt', {required: false, maxLength: 50})}
+                               className="border border-gray-400 p-2 rounded"
+                               placeholder="Apt / Unit / Suite / etc. (optional)" type="text"/>
+                    </div>
+                    <div className="grid grid-cols-4 gap-4 mb-4">
+                        <input {...register('phy_city', {required: true, maxLength: 50})}
+                               className="border border-gray-400 p-2 rounded" placeholder="City" type="text"/>
+                        <select {...register('phy_city1', {required: true, maxLength: 50})}
+                                className="border border-gray-400 p-2 rounded">
+                            <option>
+                                Alabama
+                            </option>
+                        </select>
+                        <input {...register('phy_zipcode', {required: true, maxLength: 50})}
+                               className="border border-gray-400 p-2 rounded" placeholder="Zipcode" type="text"/>
+                        <input {...register('phy_phone', {required: true, maxLength: 50})}
+                               className="border border-gray-400 p-2 rounded" placeholder="Phone" type="text"/>
+                    </div>
+                    <div className="mb-4">
+                        <input {...register('save_ship', {required: false})} className="mr-2"
+                               type="checkbox"/>
+                        Save Ship From Address
+                        <input {...register('ship_nickname', {required: true, maxLength: 50})}
+                               className="border border-gray-400 p-2 rounded ml-2"
+                               placeholder="Nickname this Ship From Address" type="text"/>
+                    </div>
+                </div>}
+
 
                 <div className={"flex justify-between border-t pt-4 items-start w-full"}>
-                    <Toggle unit={unit} setUnit={setUnit}/>
-                    <button type={"button"} onClick={handleAddPackage}
-                            className={'font-semibold bg-blue-600 hover:bg-blue-700 p-2 text-white rounded'}>ADD Package
-                    </button>
+                    <h2 className="text-2xl font-bold mb-4 ">
+                        Packages
+                    </h2>
+                    <div className={"flex items-center justify-center"}>
+                        <Toggle unit={unit} setUnit={setUnit}/>
+                        <button type={"button"} onClick={handleAddPackage}
+                                className={'ml-4 font-semibold bg-blue-600 hover:bg-blue-700 p-2 text-white rounded'}>ADD
+                            Package
+                        </button>
+                    </div>
+
                 </div>
                 {packages.map((item, idx) => {
-                    return (<Package unit={unit} setPackages={setPackages} index={idx} key={idx} package_item={item}/>)
+                    return (<Package len={packages.length} unit={unit} setPackages={setPackages} index={idx} key={idx}
+                                     package_item={item}/>)
                 })}
                 <h2 className="text-lg font-bold mb-4 w-full">
                     Commercial Invoice
                 </h2>
-                <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">ITEM DESCRIPTION (IN ENGLISH)</label>
+                <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">ITEM
+                    DESCRIPTION (IN ENGLISH)</label>
                 <div className="relative">
                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="search" id="search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ITEM DESCRIPTION (IN ENGLISH)" required />
+                    <input type="search" id="search"
+                           className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="ITEM DESCRIPTION (IN ENGLISH)" required/>
                 </div>
                 {/*<input*/}
                 {/*    className="border w-full border-gray-400 p-2 rounded mb-4"  placeholder="ITEM DESCRIPTION (IN ENGLISH)"*/}
@@ -218,7 +241,8 @@ const Home = ({}) => {
                     {/*<button type="button" className="bg-gray-400 text-white px-4 py-2 rounded mr-2">*/}
                     {/*    CANCEL*/}
                     {/*</button>*/}
-                    <button disabled={checkInputs()} type="submit" className="bg-blue-500 disabled:bg-gray-400 text-white px-4 py-2 rounded">
+                    <button disabled={checkInputs()} type="submit"
+                            className="bg-blue-500 disabled:bg-gray-400 text-white px-4 py-2 rounded">
                         SAVE
                     </button>
                 </div>
