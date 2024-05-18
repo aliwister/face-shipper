0df56f99f0376f881ff31a4b5fc1e4cf5ed894b8
Ebi <tmpmailebi@gmail.com>
2024-05-18 21:28:42 +0330

    now caching token

--- pages/api/fedex_util.js
+++ pages/api/fedex_util.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
 import qs from "qs";
 
+let authToken = {
+    token:null,
+    expires_at:null
+}
+
 export async function getAccessToken(){
+
+    if(authToken.token && authToken.expires_at > new Date().getTime())
+        return authToken.token
     const url = `${process.env.FEDEX_URL}/oauth/token`
     const config = {
         method: 'POST',
@@ -17,6 +25,8 @@ export async function getAccessToken(){
     }
 
     const {data} = await axios(config)
+    authToken.token = data.access_token
+    authToken.expires_at = new Date().getTime() + data.expires_in * 1000 * 60 * 60
     return data.access_token
 
 }
