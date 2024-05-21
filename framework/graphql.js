7a14cc56d16a2d5019ed6231be7c3676f1baf087
mjkohan <mjk1551381@gmail.com>
2024-05-21 23:44:39 +0330

    create label first working version(todo:check fields,error handling,change order state,check if label exists dont generate again)

--- framework/graphql.js
+++ framework/graphql.js
@@ -1,9 +1,65 @@
-export const CART = /* GraphQL */ `query cart($secureKey: String) {
-    cart(secureKey: $secureKey) {
+export const CART = /* GraphQL */ `query cart($secureKey: String) {     
+    cart(secureKey: $secureKey) {        
         secureKey
         currency
         gift
-        additionalInfo
+        additionalInfo {
+            sender_state
+            price
+            date
+            zipcode
+            sender_zipcode
+            email
+            name
+            company
+            city
+            state
+            address
+            address_opt
+            sender_name
+            sender_company
+            sender_address
+            sender_address_opt
+            sender_city
+            sender_zipcode
+            sender_phone
+            receiver_phone
+            receiver_countryCode
+            items {
+                name
+                description
+                harmonizedCode
+                countryOfManufacture
+                quantity
+                quantityUnits
+                weight {
+                    value
+                    units
+                }
+                customsValue {
+                    amount
+                    currency
+                }
+                partNumber
+                numberOfPieces
+                unitPrice {
+                    amount
+                    currency
+                }
+            }
+            requestedPackageLineItems {
+                weight {
+                    value
+                    units
+                }
+                dimensions {
+                    length
+                    width
+                    height
+                    units
+                }
+            }
+        }
         cartItems {
             id
             api
@@ -17,16 +73,48 @@ export const CART = /* GraphQL */ `query cart($secureKey: String) {
             salePrice
             currency
             slug
-            unit
+            unit         
         }
+        adjustments {
+            discountReductionType
+            discount {
+                base
+                prices
+            }
+        }
+        cartRule {
+            coupon
+            autoApply
+            canCombine
+            enabled
+            priority
+            description {
+                lang
+                value
+            }
+            rules {
+                minCartSize
+            }
+            checkoutRules {
+                excludedCarriers
+                excludedPayments
+            }
+            discountType
+            reductionType
+            discount {
+                base
+                prices
+            }
+        }  
     }
-}`;
+}
+`;
 export const UPDATE_TENANT_CART_MUTATION = /* GraphQL */ `
   mutation updateTenantCart(
     $secureKey: String
     $items: [CartItemInput]
     $isMerge: Boolean
-    $additional_info: String
+    $additional_info: AdditionalInfoInputDto
   ) {
     updateTenantCart(
       secureKey: $secureKey
@@ -38,7 +126,6 @@ export const UPDATE_TENANT_CART_MUTATION = /* GraphQL */ `
         secureKey
         currency
         gift
-        additionalInfo
         cartItems {
           id
           api
@@ -130,23 +217,80 @@ export const PROCESS_PAYMENT = `
     }
   }
 `;
-export const ORDER_CONFIRMATION= `query orderConfirmation($paymentKey: String) {
+export const ORDER_CONFIRMATION = `query orderConfirmation($paymentKey: String) {
   orderConfirmation(paymentKey: $paymentKey) {
     id
     reference
     confirmationKey
   }
 }`;
-export const ORDER_HISTORY= `query orderHistory($state: [OrderState], $offset: Int, $limit: Int) {
+export const ORDER_HISTORY = `query orderHistory($state: [OrderState], $offset: Int, $limit: Int) {
   orderHistory(state: $state, offset: $offset, limit: $limit) {
     items{
-        additionalInfo
+        id
+        additionalInfo {
+            sender_state
+            price
+            date
+            zipcode
+            sender_zipcode
+            email
+            name
+            company
+            city
+            state
+            address
+            address_opt
+            sender_name
+            sender_company
+            sender_address
+            sender_address_opt
+            sender_city
+            sender_zipcode
+            sender_phone
+            receiver_phone
+            receiver_countryCode
+            items {
+                name
+                description
+                harmonizedCode
+                countryOfManufacture
+                quantity
+                quantityUnits
+                weight {
+                    value
+                    units
+                }
+                customsValue {
+                    amount
+                    currency
+                }
+                partNumber
+                numberOfPieces
+                unitPrice {
+                    amount
+                    currency
+                }
+            }
+            requestedPackageLineItems {
+                weight {
+                    value
+                    units
+                }
+                dimensions {
+                    length
+                    width
+                    height
+                    units
+                }
+            }
+        }
     }    
     total
     hasMore
   }
 }`;
-export const CREATE_SHIPMENT= `mutation CreateShipment($shipment: ShipmentInput!, $shipmentItems: [ShipmentItemInput]) {
+export const CREATE_SHIPMENT = `mutation CreateShipment($shipment: ShipmentInput!, $shipmentItems: [ShipmentItemInput]) {
   createShipment(shipment: $shipment, shipmentItems: $shipmentItems) {
     id
     estimatedShipDate
@@ -197,7 +341,7 @@ export const CREATE_SHIPMENT= `mutation CreateShipment($shipment: ShipmentInput!
   }
 }`;
 
-export const ADD_SHIPMENT_DOC= `mutation AddShipmentDoc($id:Long, $filename: String) {
+export const ADD_SHIPMENT_DOC = `mutation AddShipmentDoc($id:Long, $filename: String) {
   addShipmentDoc(id:$id, filename: $filename) {
     value
   }
